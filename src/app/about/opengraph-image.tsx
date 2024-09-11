994db3c978ad3e52f100d3cf85e0c154348b40e4
Re9iNee <attarzadeh76@gmail.com>
2024-09-11 19:48:36 +0330

    adding image

--- src/app/about/opengraph-image.tsx
+++ src/app/about/opengraph-image.tsx
@@ -3,7 +3,7 @@ import { ImageResponse } from "next/og";
 export const runtime = "edge";
 
 // Image metadata
-export const alt = "About Us Page";
+export const alt = "About Acme";
 export const size = {
   width: 1200,
   height: 630,
@@ -32,7 +32,7 @@ export default async function Image() {
           justifyContent: "center",
         }}
       >
-        About Us
+        ☠ About Us ☠
       </div>
     ),
     // ImageResponse options
